e8822317c356eac06cdcad532d4b53448d626816
nilesh221098 <nileshmishraakbarpur@gmail.com>
2024-09-02 01:50:42 +0530

    update

--- scopes.js
+++ scopes.js
@@ -7,10 +7,49 @@ let a = 500
 if(true) {
     let a  = 10
     const b = 20
-    console.log("inner", a)
+    //console.log("inner", a)
     
 }
 
 //console.log(a)
 //console.log(b)
-console.log(a)
+//console.log(a)
+
+
+function one(){
+    const username = "nilesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website)
+
+   // two()
+}
+//one()
+
+
+if(true){
+    const username = "nilesh"
+    if(username === "nilesh"){
+        const website = " youtube"
+        //console.log(username + website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+
+// ========= imp intresting-----------------
+
+//addone(5) working
+function addone(num){
+return num + 1
+}
+addone(5)
+
+//addtwo(5) not working
+const addtwo = function(num){
+    return num + 3
+}
+addtwo(5)
