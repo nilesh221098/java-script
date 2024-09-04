68adb396ec39cf68820b4a84624ad6ec2e7c1f4f
nilesh221098 <nileshmishraakbarpur@gmail.com>
2024-09-05 01:26:54 +0530

    update

---
+++ control_flow.js
@@ -0,0 +1,88 @@
+// if
+
+//if (true){
+//    code executed
+//}
+
+//if (false){
+ //   code not executed
+//}
+
+//const userloggedin = true
+
+//if (userloggedin){
+//console.log("hi")
+//}
+
+//<, >, <=, >=, ==, != , === !===
+
+//const bal = 100
+//if(bal > 80) console.log("test")
+
+//const bal = 100
+
+ //if (bal > 80){
+ //   console.log("less")
+//} else if (bal < 100) {
+ //   console.log("greater")
+//} else {
+//    console.log("def")
+//}
+
+
+//const month = 3
+//switch(month){
+//case 1:
+ //   console.log("january")
+//    break;
+//case 2:
+ //   console.log("march")
+ //   break;
+//case 3:
+    //console.log("april")
+ //  break;
+//default:
+//    console.log("def")
+//    break;
+//}
+
+//const useremail = "n@nilesh.com"
+//if(useremail){
+//    console.log("got user email");
+//} else {
+//    console.log("not");
+//}
+
+
+// false value - false , 0, -0, biging 0n, "", null, undefined
+// NaN
+
+
+// true value - "0", 'false', " ", [], {}, function(){}
+//const useremail = []
+//if (useremail.length===0){
+//console.log("empty")
+//}
+
+//const emptyobj = {}
+//if (Object.keys(emptyobj).length===0){
+//    console.log("empty")
+//    }
+
+
+//Nullish Caolescing operator (??):null undefined
+//let val1 = 5;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+//val1 = null ?? 10 ?? 15
+//console.log(val1);
+
+// ternary operator
+
+//condition ? true: false
+
+const price = 100
+price >= 80 ? console.log("less"): console.log("greater")
+
+
