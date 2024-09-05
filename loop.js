396f68c054b12fab82dbb8d549b9d68375dceac1
nilesh221098 <nileshmishraakbarpur@gmail.com>
2024-09-05 23:24:18 +0530

    update

---
+++ loop.js
@@ -0,0 +1,96 @@
+// for 
+
+//for (let index = 0; index <= 10; index++) {
+ //   const element = index;
+ //   console.log(element)  
+//}
+
+//console.log()
+
+const myarray  = [1,2,3,4,5]
+for(const num of myarray){
+    //console.log(num)
+}
+
+const greeting  = "hello world"
+for (const gr of greeting){
+    //console.log(`each char is: ${gr}`)
+}
+
+//maps
+
+const map = new Map()
+map.set('in',"india")
+map.set('usa',"united s of a")
+//console.log(map)
+
+for (const key of map) {
+    //console.log(key)
+    
+}
+
+for (const [key, value] of map) {
+    //console.log(key, ':', value)
+}
+
+//not working in object iteration
+//const myobject {
+//    game1:'nfs',
+//    game2:'spr'
+//}
+//for (const [key, value] of myobject) {
+//    console.log(key, ':', value)
+//}
+
+const myobject = {
+    js:'javascript',
+    cpp:'c++'
+}
+for (const key in myobject) {
+    //console.log(myobject[key])
+}
+
+const pro = ["js","rb","py"]
+for (const key in pro) {
+    //console.log(pro[key])
+}
+
+//for each loop
+const cd = ["js", "py","java"]
+//cd.forEach(function (item) {
+ //   console.log(item)
+//});
+
+//cd.forEach( (item) => {
+//    console.log(item)
+//});
+
+//function printMe (item){
+ //   console.log(item)
+//}
+//cd.forEach(printMe)
+
+//cd.forEach((item, index, arr)=> {
+//    console.log(item,index,arr)
+//})
+
+const mycd = [
+    {
+        lname:"js",
+        lfile:"jsf",
+    },
+    {
+        lname:"java",
+        lfile:"jsf",
+    },
+    {
+        lname:"py",
+        lfile:"pyf",
+    }
+]
+mycd.forEach((item)=>{
+    console.log(item.lname)
+})
+
+
+
