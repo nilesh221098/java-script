c0503bb3807aaa21d40657cc2ecdf95ccd3a51b1
nilesh221098 <nileshmishraakbarpur@gmail.com>
2024-09-24 01:32:54 +0530

    add

--- oops/classes.js
+++ oops/classes.js
@@ -15,10 +15,23 @@ class user {
     }
 }
 
-const chai = new user ("chai", "abc@.com", "123")
-console.log(chai.encryptPassword())
-console.log(chai.changeuser())
+//const chai = new user ("chai", "abc@.com", "123")
+//console.log(chai.encryptPassword())
+//console.log(chai.changeuser())
 
 
 
 //behind the seen
+
+function user(username, email, pass){
+    this.username = username
+    this.email = email
+    this.pass = pass
+}
+user.prototype.encryptPassword = function(){
+    return `${this.pass}abc`
+}
+
+const tea = new user ("chai", "abc@.com", "123")
+console.log(tea.encryptPassword())
+console.log(tea.changeuser())
